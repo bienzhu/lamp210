65e773adc9bef48c060e3950ae4f5425a7f7687a
bienzhu <14299792@qq.com>
2018-09-20 16:32:17 +0800

    后台,贴子

--- admin.php
+++ admin.php
@@ -1,2 +1,12 @@
 <?php
 	echo 'admin page';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		后台
+	</body>
+</html>
--- post.php
+++ post.php
@@ -1,2 +1,12 @@
 <?php
 	echo 'post page';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		发贴
+	</body>
+</html>
